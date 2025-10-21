aa66ff7ad6b68ce198eb0d270da324c8a64416ad
S540d <S540d@users.noreply.github.com>
2025-10-21 17:05:34 +0200

    chore: Bump version to 1.0.1 for resubmission to Google Play
    - Increase versionCode from 1 to 2 in build.gradle.kts
    - Increase versionName from 1.0 to 1.0.1 in build.gradle.kts
    - Update package.json version to 1.0.1
    - New .aab will be generated for Google Play Store resubmission with improved assets

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.sven4321.trainer1x1"
         minSdk = 21
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
